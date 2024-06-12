a9c44ac0b43ae0474909e0240d23b67217208c11
achaltelmasre <achaltelmasre@gmail.com>
2024-06-12 16:06:17 +0530

    create data register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');   
+  const [mobile, setMobile] = useState('');
+  const [address, setAddress] = useState('');
+  const [gender, setGender] = useState('female');
+
+  
+  const signup = async () => {
+
+      if (!name || !email || !password || !mobile || !address || !gender) 
+      {
+         alert('Please enter all fields')
+         return 
+      }
+   
+      const response = await axios.post("/signup", {
+            
+              name:name,
+              email:email,
+              password:password,
+              mobile:mobile,
+              address:address,
+              gender:gender
+          })
+
+          alert(response?.data?.message);
+
+          if (response?.data?.success) {
+              alert(response?.data?.message);
+              window.location.href = "/login";
+          }
+  };
+
+  useEffect(() =>{
+    const storageUser = JSON.parse(localStorage.getItem("user") || '{}');
+
+    // if (storageUser?.email) {
+    //    alert("You are already logged in!");
+    //    window.location.href = "/";
+    // }
+  }, [])
+
+  return(
+    <div className="signup">
+  
+      <form className="main-container">
+         <h1 className="text-center"> Register form</h1>
+
+         <div>
+          <lable htmlFor='name'>Name:</lable>
+          <input type='text' placeholder="Enter your name" id='name' value={name}
+          className="input-form"
+           onChange={(e) => {setName(e.target.value)}}
+          />
+         </div>
+
+         <div>
+          <lable htmlFor='email'>Email:</lable>
+          <input type='email' 
+            placeholder="Enter your email" 
+            id='email' value={email}
+            className="input-form"
+            onChange={(e) => {setEmail(e.target.value)}}/>
+         </div>
+
+         <div>
+          <lable htmlFor='password'>Password:</lable>
+          <input type='password' 
+            placeholder="Enter your password" 
+            id='password' value={password}
+            className="input-form"
+            onChange={(e) => {setPassword(e.target.value)}}/>
+         </div>
+
+         <div>
+          <lable htmlFor='mobile'>MobileNo.:</lable>
+          <input type='text' 
+            placeholder="Enter your mobile number"
+            id='mobile' value={mobile}
+            className="input-form"
+            onChange={(e) => {setMobile(e.target.value)}}/>
+         </div>
+
+         <div>
+          <lable htmlFor='address'>Address:</lable>
+          <input type='text'
+            placeholder="Enter your address"
+            id='address' value={address}
+            className="input-form"
+            onChange={(e) => {setAddress(e.target.value)}}/>
+         </div>
+
+         <div>
+          
+          <input type="radio"
+               name="gender"
+                id="male" 
+                className="gender" 
+                checked={gender === "male"} 
+                onClick={(e) => { setGender("male")}}/>
+          <label htmlFor="male">Male </label>
+
+          <input type="radio" name="gender"     
+                 id="female" 
+                 className="gender" 
+                 checked={gender === "female"}
+                 onClick={(e) => { setGender("female")}}/>
+          <label htmlFor="female">Female </label>
+         </div>
+
+         <button type="button" className="btn signup-btn"
+          onClick={signup}>
+           Register
+         </button>
+
+        <p className="text-center">
+        {/* <Link to="../login" className="address-link" >Already have Account</Link> */}
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         
+
+       </form>
     </div>
-  );
+   
+  
+      )
 }
 
 export default App;
